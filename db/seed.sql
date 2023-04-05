76597cb718836e1d61f7b47aea508cc1ef62ed79
treecoww <tcao89@gmail.com>
2023-04-04 19:00:10 -0500

    update seeds

--- db/seed.sql
+++ db/seed.sql
@@ -1,9 +1,9 @@
 -- starter seeds for dept, role, & emp
 INSERT INTO department(name)
-VALUES ("Engineering"),("Support");
+VALUES ("Engineering"),("Support"),("HR"),("Sales");
 
-INSERT INTO role (job_title, salary, department_id)
-VALUES ("Manager", 100000, 1),("Back End Engineer", 100000, 2);
+INSERT INTO roles(title, salary, department_id)
+VALUES ("Engineering Manager", 100000, 1),("Back End Engineer", 100000, 1),("Account Manager", 80000, 4),("Support Agent", 75000, 2),("HR Rep", 80000, 3);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tree", "Cao", 1, NULL),("Umatr","Tome", 2, 1);
+INSERT INTO employees(first_name, last_name, role_id, manager_id)
+VALUES ("Tree", "Cao", 1, NULL),("Matt","When", 2, 1),("Ro", "Gonzo", 4, 1), ("Rita", "Raja", 3, NULL);
